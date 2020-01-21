6c7e900da9fd88632cd98feb49ff50463fb47ef1
Kevin Deems <kevin8deems@gmail.com>
2020-01-21 21:11:36 +0000

    Added setup script to repo

---
+++ ruby/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "This is the version of ruby you are using:"
+ruby -v
+echo
+
+echo "Ruby is located at:"
+which ruby
+echo
+
+echo "Run the client with 'ruby helloRuby.rb <server IP> <port number>'"
+echo "Server can be run with 'ruby hellosrvRuby.rb <port number>'"
